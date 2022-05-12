823b38d3a41bd753b1ca7cb639f3318c043a1ab3
Vitória Souza <viihsouza161@gmail.com>
2022-05-11 21:14:22 -0300

    FinAPI: added get and post methods

--- chapter-I/FinAPI/src/index.js
+++ chapter-I/FinAPI/src/index.js
@@ -1,6 +1,40 @@
 import express from 'express'
+import { v4 as uuidv4 } from 'uuid'
 
 const app = express()
 app.use(express.json())
+
+const customers = []
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+  if (customerAlreadyExists) res.status(400).send({ error: 'customer already exists' })
+
+  customers.push({
+    name,
+    cpf,
+    id: uuidv4(),
+    statement: []
+  })
+
+  res.status(201).send(customers)
+})
+
+app.get('/statement', (req, res) => {
+  const { cpf } = req.headers
+  const customer = customers.find((current) => current.cpf === cpf)
+
+  if (!customer) {
+    return res.status(404).send({ error: 'Customer not found' })
+  }
+
+  if (customer.statement.length === 0) {
+    return res.status(204).send(customer.statement)
+  }
+
+  res.status(200).send(customer.statement)
+})
 
 app.listen(3333, () => console.log('Running'))
