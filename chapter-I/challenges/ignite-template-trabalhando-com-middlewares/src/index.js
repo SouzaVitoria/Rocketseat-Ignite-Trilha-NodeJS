42862801ffef7d17d7f3118ca3b7eddb9312e63b
Vitória Souza <viihsouza161@gmail.com>
2022-05-14 17:13:15 -0300

    ChallengerMiddlewares: added checksExistsUserAccount middleware

--- chapter-I/challenges/ignite-template-trabalhando-com-middlewares/src/index.js
+++ chapter-I/challenges/ignite-template-trabalhando-com-middlewares/src/index.js
@@ -10,7 +10,15 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const userExists = users.find((user) => user.username === username);
+
+  if (userExists) {
+    request.user = userExists
+    return next()
+  }
+
+  return response.status(404).json({ error: 'User not found' });
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
