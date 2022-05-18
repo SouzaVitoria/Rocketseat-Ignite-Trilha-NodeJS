6b49647fb997135538e72508a3a287ccf9a12509
Vitória Souza <viihsouza161@gmail.com>
2022-05-18 19:48:40 -0300

    ChapterIIRentalX: added get method - categories

--- chapter-II/rentalx/src/routes/categories.routes.ts
+++ chapter-II/rentalx/src/routes/categories.routes.ts
@@ -11,4 +11,9 @@ categoriesRoutes.post("/", (request, response) => {
   return response.status(201).send()
 })
 
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list()
+  return response.status(200).json(all)
+})
+
 export { categoriesRoutes }
